2305f73e753d05aa45287d3cb0f9db95f5f1f312
HeekunLim <fanghk555@gmail.com>
2024-04-13 01:03:48 +0900

    버튼에서 자동으로 출력하게 변경, 광고 추가

--- git_issue/src/pages/list.jsx
+++ git_issue/src/pages/list.jsx
@@ -1,23 +1,55 @@
-// import instance from "/network/axios";
 import Axios from "axios";
 import React from "react";
+import Cat from "./IMG_2126.jpg";
 
 const List = () => {
-    const [data, setData] = React.useState();
+    // number, title, user.login, comments, created_at
+    const [gitIssue, setGitIssue] = React.useState([]);
 
-    const onClick = () => {
+    React.useEffect(()=>{
         Axios
-          .get("https://api.github.com/repos/angular/angular-cli/issues?sort=comments-desc")
-          .then((response) => {
-            setData(response.data);
-          });
-      };
+            .get("https://api.github.com/repos/angular/angular-cli/issues?sort=comments-desc", {
+                headers: {Authorization: "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"},
+            })
+            .then((response) => {
+                for (let i = 0; i < response.data.length; i++){
+                    if (i === 4){
+                        let AD = {};
+                        AD.id = "cat";
+                        setGitIssue(old => [...old, AD]);
+                    }
+
+                    let newData = {};
+                    newData = {};
+                    newData.id = i;
+                    newData.number = response.data[i].number;
+                    newData.title  = response.data[i].title;
+                    newData.login = response.data[i].user.login;
+                    newData.comments = response.data[i].comments;
+                    newData.created_at = response.data[i].created_at;
+
+                    setGitIssue(old => [...old, newData]);
+                }
+
+                console.log(JSON.stringify(gitIssue));
+            });
+    }, [])
 
     return (
         <div>
             <h1>리스트</h1>
-            <button onClick={onClick}>불러오기</button>
-            {JSON.stringify(data)}
+            {gitIssue.map((gitIssue) => {
+                if (gitIssue.id === "cat"){
+                    return(
+                        <a href="https://www.wanted.co.kr/">
+                            <img src={Cat} alt="" style={{width: "200px"}}/>
+                        </a>                      
+                    )
+                }
+                else {
+                    return <p key={gitIssue.id + "issue"}>{JSON.stringify(gitIssue)}</p>
+                }
+            })}
         </div>
     );
 };
